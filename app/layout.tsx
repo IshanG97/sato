8edefb7efa549755640f55a508174a42bd2ba1b1
IshanG97 <ishangodawatta@gmail.com>
2025-05-11 23:24:49 +0000

    refactor: consolidate logo component import structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
